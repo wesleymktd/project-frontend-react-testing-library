eb1f86e1d787b2bc93721aba94e009874ca9319f
wesleymktd <wesleymktd@gmail.com>
2022-11-23 04:32:38 -0300

    req3

--- src/tests/FavoritePokemon.test.js
+++ src/tests/FavoritePokemon.test.js
@@ -1,1 +1,14 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import React from 'react';
+import { FavoritePokemon } from '../pages';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Testa o Component Favorite...', () => {
+  it('Topo da aplicação contém conjunto de links de navegação', () => {
+    renderWithRouter(<FavoritePokemon />);
+    // acessar
+    const textEl = screen.getByText(/no favorite pokémon found/i);
+    // testar
+    expect(textEl).toBeInTheDocument();
+  });
+});
